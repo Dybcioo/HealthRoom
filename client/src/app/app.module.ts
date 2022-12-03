8ac2282cc4049cce932ae1a23eaa920b7ae42a21
Dominik Dybowski <dybowski.dominikk@gmail.com>
2022-12-03 15:34:43 +0100

    change structure of project

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,24 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
-import { HeroComponent } from './hero/hero.component';
-import { HeaderComponent } from './header/header.component';
-import { HeroMoreComponent } from './hero-more/hero-more.component';
-import { TreatmentsComponent } from './treatments/treatments.component';
-import { TreatmentsCardComponent } from './treatments/treatments-card/treatments-card.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { CarouselItemComponent } from './carousel/carousel-item/carousel-item.component';
-import { CarouselMenuComponent } from './carousel/carousel-menu/carousel-menu.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroMoreComponent } from './components/hero-more/hero-more.component';
+import { TreatmentsComponent } from './components/treatments/treatments.component';
+import { TreatmentsCardComponent } from './components/treatments/treatments-card/treatments-card.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CarouselItemComponent } from './components/carousel/carousel-item/carousel-item.component';
+import { CarouselMenuComponent } from './components/carousel/carousel-menu/carousel-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContactComponent } from './contact/contact.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { NgxCaptchaModule } from 'ngx-captcha';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
